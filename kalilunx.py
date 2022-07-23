67ae2368a3acaf08b498930958791d850252ab74
ISSAMSARAEXE <109853679+ISSAMSARAEXE@users.noreply.github.com>
2022-07-23 18:12:55 +0100

    Add files via upload

---
+++ kalilunx.py
@@ -0,0 +1,50 @@
+import os
+import time
+import sys
+#os.system('pkg install figlet')
+os.system('clear')
+v ='\033[0;35m'
+W  = '\33[0m'
+R  = '\33[1;31m'
+G  = '\33[1;32m'
+O  = '\33[1;33m'
+B  = '\33[1;34m'
+P  = '\33[1;35m'
+C  = '\33[1;36m'
+GR = '\33[1;37m'
+rs = '\033[0;101m'
+b_b = '\033[4;31m'
+cc = '\033[0;91m'
+pp = '\033[1;92m'
+mc = 'please wait ... '
+def ex(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(220. / 200)
+print(G+'please wait ... ')
+ex ('''''')
+os.system('clear')
+def e(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1. / 50)
+e('klai lunx')
+print(rs)
+print(G)
+os.system('figlet KALI LUNX')
+print(B)
+d=('install klai linx termux ')
+gg=(v+'''
+#############################################
+# issam exe sara exe hacker                 #         
+# telegram : https://t.me/+e70FCHAYGzg0ZDA0 #         
+#############################################
+''')
+print(d)
+print(gg)
+xxp=input(G+'enter-$')
+os.system('wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/de-apt-xfce4.sh && bash de-apt-xfce4.sh')
+
+
